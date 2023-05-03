69691f09dd27da4b162e7ed8f3ff956c85dbe3fc
Ariiychyk Denis <areychyk@mail.ru>
2023-05-03 22:58:54 +0300

    auth and register

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,23 +3,58 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "app/hooks";
 import { appActions } from "app/app.slice";
 
-function App() {
-  const isLoading = useAppSelector((state) => state.app.isLoading);
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    setTimeout(() => {
-      dispatch(appActions.setIsLoading({ isLoading: false }));
-    }, 3000);
-  }, []);
-
+export default function App() {
   return (
-    <div className="App">
-      {isLoading && <h1>Loader...</h1>}
-      <Counter />
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
-export default App;
+
+
+// function App() {
+//   const isLoading = useAppSelector((state) => state.app.isLoading);
+//
+//   const dispatch = useAppDispatch();
+//
+//   useEffect(() => {
+//     setTimeout(() => {
+//       dispatch(appActions.setIsLoading({ isLoading: false }));
+//     }, 3000);
+//   }, []);
+//
+//   return (
+//     <div className="App">
+//       {isLoading && <h1>Loader...</h1>}
+//       <Counter />
+//     </div>
+//   );
+// }
+//
+// export default App;
